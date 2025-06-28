ca2e2522666d9c2c3dc452e54613c2249d4ba5b2
ShahdYahya <shahd.yahya2003@gmail.com>
2025-06-28 17:24:18 +0300

    Update part1.js

--- part1.js
+++ part1.js
@@ -72,7 +72,7 @@ async function explainResults() {
   `;
 
   try {
-    const response = await fetch('http://localhost:3000/explain', {
+     const response = await fetch('https://wireless-webproject.onrender.com/explain', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(lastCalculation),
